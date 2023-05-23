71ff8620a0d4513d1a5c016bc05d1763afc44429
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-23 07:59:35 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Znvfh
+admin.site.register(Znvfh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Znvfh(models.Model):
+    'Generated Model'
+    rryru = models.BigIntegerField()
